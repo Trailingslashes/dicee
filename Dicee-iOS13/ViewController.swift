bf4fb37040cfe68c26a6f2e23d1d121d3f5f7e34
Cole Phillips <colephillips1@gmail.com>
2022-10-19 21:25:06 -0400

    rounded button, added dice connections to main storyboard

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Roll dice button connection
+    @IBOutlet weak var Button: UIButton!
+    // First Dice in main storyboard (left side) connection
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    // Second Dice in main storyboard (right side) connection
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Load Dice six
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix.png")
+        
+        // Load Dice two
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo.png")
+        
+        // Round Button
+        Button.layer.cornerRadius = 10
+        Button.clipsToBounds = true
     }
 }
+
 
